4c8063378015e6d3aec3801ec6bbfa5c99ff5542
Viraj Kheni <Kheni_Viraj@outlook.com>
2022-02-18 13:42:10 +0530

    Add project files.

---
+++ MyDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyDemo.Models;
+
+namespace MyDemo.Data
+{
+    public class ApplicationDbContext
+        : DbContext
+    {
+
+        public DbSet<Department> Departments { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ MyDemo/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyDemo.Models
+{
+    [Table("Departments")]
+    public class Department
+    {
+        [Display(Name = "Department ID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DepartmentId { get; set; }
+
+        [Display(Name = "Name Of Department")]
+        [Required(ErrorMessage = "{0} cannot be empty")]
+        [StringLength(50, ErrorMessage = "{0} cannot be more than {1} characters")]
+        [MinLength(3, ErrorMessage = "{0} should have at least {1} characters.")]
+        public string DepartmentName { get; set; }
+
+    }
+}
